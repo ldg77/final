18759ca936e3d0d44695b4cbec428d8f0c8dcee7
Dumitru Gurduza <dimulya@gmail.com>
2023-02-21 15:13:16 +0100

    add some logic

--- components/Layout/DrawFrontItems/ShopItem.tsx
+++ components/Layout/DrawFrontItems/ShopItem.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import fetcher from "@/lib/fetcher";
+import { useSession } from "next-auth/react";
+import useSWR from "swr";
 type Prop = {
   itemdata: any;
 };
 function ShopItem({ itemdata }: Prop) {
+  const { data: session } = useSession();
+  const { data, error, isLoading } = useSWR(
+    `/api/user/path/${session?.user?.email}/shopitem`,
+    fetcher
+  );
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  console.log(data);
+
   return (
-    <div style={{ ...itemdata }} className="h-full">
-      ShopItem
+    <div style={{ ...itemdata }} className="h-full flex gap-2">
+      {data?.data.shopitem.map((item: any) => (
+        <div className="flex-1">
+          <img src={item.avatar} alt="avatar" className="w-24 aspect-square" />
+          <p>{item.productName}</p>
+          <p>price: {item.price}€</p>
+          <button className="bg-green-900 px-3 rounded text-white">add</button>
+        </div>
+      ))}
     </div>
   );
 }
